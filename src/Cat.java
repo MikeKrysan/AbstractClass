ea9d5b8b721b1d9a57ca60384bb3b0e0ccde7a3b
MikeKrysan <krysanmihasan@gmail.com>
2022-01-04 18:34:46 +0200

    first commit

---
+++ src/Cat.java
@@ -0,0 +1,10 @@
+public class Cat extends Animal{
+//    public void makeSound() {
+//        System.out.println("Meow...");
+//    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Meow...");
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args) {
+//        Cat cat = new Cat();
+//        Dog dog = new Dog();
+////        Animal animal = new Animal();
+//        dog.makeSound();
+//        cat.makeSound();
+//        cat.eat();
+//        dog.eat();
+    }
+}
